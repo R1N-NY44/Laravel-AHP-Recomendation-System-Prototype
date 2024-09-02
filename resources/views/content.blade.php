f05385bd13abd724bcd5bdb4c07ffafa08314458
Yoga <im.brindawan@gmail.com>
2024-09-02 12:31:47 +0700

    Feat : Prototype Design

--- resources/views/content.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <div class="relative overflow-x-auto">
-        <div class="items-center justify-center gap-2 overflow-x-hidden bg-black p-10">
-            <button class="btn" onclick="my_modal_4.showModal()">open modal</button>
-        </div>
-    </div>
-</div>
---
+++ resources/views/layout/content.blade.php
@@ -0,0 +1,13 @@
+<div>
+
+    <button class="btn" onclick="my_modal_4.showModal()">open modal</button>
+
+    <div>
+        @include('table.tb_lowongan')
+    </div>
+
+    <div>
+        @include('table.tb_internship')
+    </div>
+
+</div>
