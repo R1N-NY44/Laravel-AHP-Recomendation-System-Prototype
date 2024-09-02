936b4448b9636fb4cf9e2ae04594d0e5a20d3ab2
Yoga <im.brindawan@gmail.com>
2024-09-02 17:37:13 +0700

    Feat : Prototype FE 80%

--- resources/views/modals/main_modals.blade.php
+++ resources/views/modals/main_modals.blade.php
@@ -1,16 +1,5 @@
 <div>
     @include('modals.import_alter')
-    @include('modals.import_lowongan')
-    <dialog id="my_modal_4" class="modal">
-        <div class="modal-box w-11/12 max-w-[90%]">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Click the button below to close</p>
-            <div class="modal-action">
-                <form method="dialog">
-                    <!-- if there is a button, it will close the modal -->
-                    <button class="btn">Close</button>
-                </form>
-            </div>
-        </div>
-    </dialog>
+    @include('modals.add_lowongan')
+    @include('modals.add_master_course')
 </div>
