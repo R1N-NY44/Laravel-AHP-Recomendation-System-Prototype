bfc2c7d0bc726de947061f321831dff6940c1abb
Yoga <im.brindawan@gmail.com>
2024-09-02 18:19:17 +0700

    Feat : AHP Services & FE Improvement

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -1,4 +1,5 @@
 <div>
     <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    <input type="checkbox" value="synthwave" class="toggle theme-controller" />
     <button class="btn" onclick="importMasterCourse.showModal()">Tambah Matakuliah</button>
 </div>
